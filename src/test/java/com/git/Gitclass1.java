9ffecc883dd15556488ad7223fec935077b5ba36
Ralph543 <blaisedurai@outlook.com>
2022-09-15 10:36:50 +0530

    Initial codes

--- src/test/java/com/git/Gitclass1.java
+++ src/test/java/com/git/Gitclass1.java
@@ -1,5 +1,19 @@
 package com.git;
 
 public class Gitclass1 {
+	
+	private void method1() {
+		System.out.println("4");
+
+	}
+	
+	private void method2() {
+		System.out.println("5");
+	}
+	
+	private void method3() {
+		System.out.println("6");
+
+	}
 
 }
